7c624a521fa3eea8e27868a807aa661e5e968576
Andres Galvis <andres.felipe99@hotmail.com>
2025-10-03 15:53:51 -0500

    fix: update supabase client import and export syntax for consistency

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
+const { createClient }  = require('@supabase/supabase-js');
 
 // Variables desde tu panel de Supabase
 const SUPABASE_URL = process.env.SUPABASE_URL || '';
 const SUPABASE_KEY = process.env.SUPABASE_KEY || '';
 
 // Crear client
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Exportar el cliente para usarlo en otros módulos
+module.exports = supabase;
